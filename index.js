dfe1097937092dfcb1f6fefef4a7826978e6ee43
VIKAS KUMAR <kumar.vikas593@gmail.com>
2023-03-27 00:34:19 +0530

    12-middleware

--- index.js
+++ index.js
@@ -1,109 +1,70 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const port = 8000;
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded());
 
-var contactList = [
-  {
-    name: "Arpan",
-    phone: "1111111111",
-  },
-  {
-    name: "Tony Stark",
-    phone: "1234567890",
-  },
-  {
-    name: "Coding Ninjas",
-    phone: "12131321321",
-  },
-];
+// middleware1
+app.use(function(req, res, next){
+   
 
-app.get("/", function (req, res) {
-  return res.render("home", {
-    title: "Contact List",
-    contact_list: contactList,
-  });
+    req.myName = "Arpan"
+    // console.log('middleware 1 called');
+    next();
 });
 
-app.get("/practice", function (req, res) {
-  return res.render("practice", {
-    title: "Let us play with ejs",
-  });
+// middleware2
+app.use(function(req, res, next){
+    
+    console.log('My name called from MW2', req.myName);
+    // console.log('middleware 2 called');
+    next();
 });
 
-app.post("/create-contact", function (req, res) {
-  //console.log(req.body);
-  // console.log(req.body.name);
-  //console.log(req.body.phone);
-  contactList.push(req.body);
-  return res.redirect("/");
-});
-
-app.listen(port, function (err) {
-  if (err) {
-    console.log("Error in running the server", err);
-  }
-  console.log("Yup!My Express Server is running on Port", port);
-});
-
-/*------------------2nd method-------------------*/
-/*
-const express = require("express");
-const path = require("path");
-const port = 8000;
-
-const app = express();
-
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded());
 
 var contactList = [
-  {
-    name: "Arpan",
-    phone: "1111111111",
-  },
-  {
-    name: "Tony Stark",
-    phone: "1234567890",
-  },
-  {
-    name: "Coding Ninjas",
-    phone: "12131321321",
-  },
-];
+    {
+        name: "Arpan",
+        phone: "1111111111"
+    },
+    {
+        name: "Tony Stark",
+        phone: "1234567890"
+    },
+    {
+        name: "Coding Ninjas",
+        phone: "12131321321"
+    }
+]
 
-app.get("/", function (req, res) {
-  return res.render("home", {
-    title: "Contact List",
-    contact_list: contactList,
-  });
+app.get('/practice', function(req, res){
+    return res.render('practice', {
+        title: "Let us play with ejs"
+    });
 });
 
-app.get("/practice", function (req, res) {
-  return res.render("practice", {
-    title: "Let us play with ejs",
-  });
-});
 
-app.post("/create-contact", function (req, res) {
-  contactList.push({
-    name: req.body.name,
-    phone: req.body.phone,
-  });
+app.get('/', function(req, res){
+    console.log('from the get route controller', req.myName);
+    return res.render('home',{
+        title: "Contact List",
+        contact_list: contactList
+    });
+})
+app.post('/create-contact', function(req, res){
  
-  return res.redirect("/");
+    contactList.push(req.body);
+    return res.redirect('/');
+
 });
 
-app.listen(port, function (err) {
-  if (err) {
-    console.log("Error in running the server", err);
-  }
-  console.log("Yup!My Express Server is running on Port", port);
-});
-*/
+app.listen(port, function(err){
+    if (err) {
+        console.log("Error in running the server", err);
+    }
+    console.log('Yup!My Server is running on Port', port);
+})
